39efbaca1198393c86c97b3825205330c49c592a
Kylie Chesner <kyliec362@gmail.com>
2019-02-27 15:31:28 -0600

    groove radar comment

--- writeSimfile.py
+++ writeSimfile.py
@@ -5,6 +5,7 @@ def writeSimfile(title="",artist="",music="",offset=-0.000000,bpms=128):
     # setting difficulty level to 1
     # setting difficulty mode to edit (instead of hard, expert, etc.)
     # setting play mode to dance-singles
+    # the series of zeros represents groove radar values; these values aren't required for now 
     chartMetadata ="#NOTES:\n\tdance-single:\n\t:\n\tEdit:\n\t1:\n\n0.0,0.0,0.0,0.0,0.0:\n"
     measures = ""
     for i in range(30): #for now im saying there are 30 measures in the song
