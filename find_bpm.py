503071dd822a7737f54645885665b03d746c4483
lukas <lukasgross2020@u.northwestern.edu>
2019-02-28 13:58:46 -0600

    Refactor codebase (black + rename)

--- find_bpm.py
+++ find_bpm.py
@@ -8,20 +8,19 @@ from madmom.features.tempo import TempoEstimationProcessor
 from madmom.features.onsets import OnsetPeakPickingProcessor, RNNOnsetProcessor
 from pydub import AudioSegment
 
-def getBPM(path):
+
+def get_bpm(path):
     proc = TempoEstimationProcessor(fps=100)
-    act = RNNBeatProcessor()(path) #returns activation function
-    tempos = proc(act) #pass activations to processor to get tempo information
-    tempo = round(tempos[0,0],4)
-    if tempo < 100: #slow songs are less fun
-        tempo*=2
+    act = RNNBeatProcessor()(path)  # returns activation function
+    tempos = proc(act)  # pass activations to processor to get tempo information
+    tempo = round(tempos[0, 0], 4)
+    if tempo < 100:  # slow songs are less fun
+        tempo *= 2
     return tempo
 
-def getBeats(path):
+
+def get_beats(path):
     proc = BeatTrackingProcessor(fps=100)
     act = RNNBeatProcessor()(path)
-    beatProbabilities = proc(act)
-    return beatProbabilities
-
-
-
+    beat_probabilities = proc(act)
+    return beat_probabilities
--- onset_detection.py
+++ onset_detection.py
@@ -9,31 +9,34 @@ from madmom.features.beats import RNNBeatProcessor
 from madmom.features.tempo import TempoEstimationProcessor
 from madmom.features.onsets import OnsetPeakPickingProcessor, RNNOnsetProcessor
 from pydub import AudioSegment
-import bpmFinder
+import find_bpm
 
-def getOnsets(path):
+
+def get_onsets(path):
     proc = OnsetPeakPickingProcessor(fps=100)
     act = RNNOnsetProcessor()(path)
     onsets = proc(act)
-    #print(16,onsets)
-    #print(16,onsets)break
+    # print(16,onsets)
+    # print(16,onsets)break
     return onsets
 
-def onsetsToNotes(onsets,bpm):
+
+def onsets_to_notes(onsets, bpm):
     notes = np.zeros(np.shape(onsets))
     for i in range(len(notes)):
-        notes[i] = int(bpm * (1/60.0) * onsets[i])
+        notes[i] = int(bpm * (1 / 60.0) * onsets[i])
     return notes
 
-def notesToMeasures(notes,BPM,path):
+
+def notes_to_measures(notes, bpm, path):
     y, sr = librosa.load(path, sr=None)
-    songLength = len(y)/sr
-    numBeats = math.floor(BPM * (1/60) * songLength)
-    chart = np.zeros((numBeats,4))
-    for i in range(numBeats):
+    song_len = len(y) / sr
+    num_beats = math.floor(bpm * (1 / 60) * song_len)
+    chart = np.zeros((num_beats, 4))
+    for i in range(num_beats):
         for j in notes[i:]:
             j = int(j)
-            r = random.randint(0,3)
+            r = random.randint(0, 3)
             if j == i:
                 chart[i][r] = 1
             else:
@@ -42,7 +45,5 @@ def notesToMeasures(notes,BPM,path):
                 break
     measures = ""
     for row in chart:
-        measures+= (str(row)[1:-1] + "\n,\n")
-    #print(measures)
+        measures += str(row)[1:-1] + "\n,\n"
     return measures[:-2]
-
