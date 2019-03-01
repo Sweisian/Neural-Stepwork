f7f37b88c675a450f0a1ddf46c89983416098ac3
lukas <lukasgross2020@u.northwestern.edu>
2019-03-01 11:03:27 -0600

    Move code into module and build run script
    Squashed commit of the following:
    
    commit e64b1677b23cc9fc07920dcc5a3f8b88f5552093
    Author: Kylie Chesner <kyliec362@gmail.com>
    Date:   Fri Mar 1 01:06:00 2019 -0600
    
        onsets to notes works!
    
    commit 7a95dd3ac297ba34c8517c761daaa4b15c41a632
    Merge: 93f4840 503071d
    Author: lukasagross <lukasgross2020@u.northwestern.edu>
    Date:   Thu Feb 28 13:59:06 2019 -0600
    
        Merge pull request #1 from Sweisian/lukas-fixup
    
        Refactor codebase (black + rename)
    
    Reformat (black)

---
+++ neural_stepwork.py
@@ -0,0 +1,48 @@
+import os
+import time
+import argparse
+from shutil import copy
+
+from neural_stepwork.simfile import write_simfile
+from neural_stepwork.find_bpm import get_bpm
+
+PREFIX = "Run_"
+OUTPUT_DIR = "output/"
+EXTENSION = ".wav"
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Create StepMania simfile.")
+    parser.add_argument(
+        "-f", "--file", required=True, dest="file", help="file or directory to parse"
+    )
+
+    args = parser.parse_args()
+
+    files = list()
+
+    if os.path.isfile(args.file):
+        files.append(args.file)
+
+    elif os.path.isdir(args.file):
+        for dirpath, _, filenames in os.walk(args.file):
+            for f in filenames:
+                if not f.endswith(EXTENSION):
+                    continue
+                files.append(os.path.abspath(os.path.join(dirpath, f)))
+
+    else:
+        raise ValueError("Unknown file {}".format(args.file))
+
+    dest = OUTPUT_DIR + PREFIX + str(int(time.time())) + "/"
+
+    if not os.path.isdir(OUTPUT_DIR + dest):
+        os.mkdir(dest)
+
+    for source_file in files:
+        copy(source_file, dest)
+        name = os.path.basename(source_file).split(".wav")[0]
+        print("Processing {}".format(name))
+
+        write_simfile(
+            title=name, music=source_file, bpms=get_bpm(source_file), dest=dest
+        )
