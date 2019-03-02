c921a18ba0c0d9157b3453fece962db0cec80b23
Kylie Chesner <kyliec362@gmail.com>
2019-03-02 11:36:27 -0600

    removed some not relevant files from training set

--- neural_stepwork/training_cleanup.py
+++ neural_stepwork/training_cleanup.py
@@ -5,41 +5,51 @@
 import os
 import soundfile as sf
 import shutil
-
 import os
 
-directory = os.fsencode("training")
 
-extentionsToRemove = ['.avi','.png','.dwi','.old','.ssc']
-audioExt = ['.ogg']
+def file_folder_cleanup():
+    directory = os.fsencode("training")
+    extentionsToRemove = ['.avi','.png','.dwi','.old','.ssc','.db','.lrc']
+    audioExt = ['.ogg']
+    for songFolders in os.scandir(directory):
 
-for songFolders in os.scandir(directory):
-    foundSM = False
-    for entry in os.scandir(songFolders.path):
-
-        if entry.is_dir():
-            shutil.rmtree(entry.path)
-        elif entry.is_file():
+        for entry in os.scandir(songFolders.path):
+            if entry.is_dir():
+                shutil.rmtree(entry.path)
+            elif entry.is_file():
+                name = (os.path.splitext(entry.name))
+                extention = name[1].decode("utf-8").lower()
+                if extention == '.mp3':
+                    shutil.rmtree(songFolders.path.decode("utf-8"))
+                    continue
+                name = str(name[0])
+                if extention in extentionsToRemove:
+                    try:
+                        os.remove(entry.path)
+                        continue
+                    except:
+                        continue
+                if extention in audioExt:
+                    data, samplerate = sf.read(entry.path.decode("utf-8"))
+                    title = entry.name.decode("utf-8").split(".")[0]
+                    sf.write(songFolders.path.decode("utf-8")+'/'+title+'.wav', data, samplerate)
+    for songFolders in os.scandir(directory):
+        foundSM = False
+        foundWav = False
+        for entry in os.scandir(songFolders.path):
             name = (os.path.splitext(entry.name))
-            extention = name[1].lower().decode("utf-8")
-            if extention == '.sm':
-                foundSM = True
-            if extention == '.mp3':
-                shutil.rmtree(songFolders.path.decode("utf-8"))
-                continue
-            name = str(name[0])
-            if extention in extentionsToRemove:
-                try:
-                    os.remove(entry.path)
-                    continue
-                except:
-                    continue
-            if extention in audioExt:
-                data, samplerate = sf.read(entry.path.decode("utf-8"))
-                title = entry.name.decode("utf-8").split(".")[0]
-                sf.write(songFolders.path.decode("utf-8")+'/'+title+'.wav', data, samplerate)
+            extention = name[1].decode("utf-8").lower()
+            if extention =='.sm':
+                foundSM=True
+            if extention == '.wav':
+                foundWav = True
+            if (extention != '.wav') and (extention != '.sm'):
+                print("remove ",entry.path)
+                os.remove(entry.path)
+        if not foundWav and not foundSM== False: #need simfile for testing/training
+            print("removing folder, need simfile and audio", "|||",songFolders.path)
+            shutil.rmtree(songFolders.path.decode("utf-8"))
 
-
-    if foundSM == False: #need simfile for testing/training
-        print("removing folder, no simfile found")
-        shutil.rmtree(songFolders.path.decode("utf-8"))
+def extract_chart_from_simfile():
+    
