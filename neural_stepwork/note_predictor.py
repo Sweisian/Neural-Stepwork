3f9691953cf6cac2911d4ceb3aabd92d38075a3f
sweisian <rswei888@gmail.com>
2019-03-16 20:14:18 -0500

    merging note_generation code to master

--- neural_stepwork/note_predictor.py
+++ neural_stepwork/note_predictor.py
@@ -4,11 +4,15 @@ from keras.layers import Dense
 from keras.layers import Dropout
 from keras.layers import LSTM
 from keras.layers import Activation
+from keras.models import model_from_json
+from onset_detection import times16ths ,get_onsets, notes_to_measures, onsets_to_notes
+from find_bpm import get_bpm
 
-def generate(onset_array, n_vocab):
+def generate(binary_onset_array, n_vocab):
     #TODO: handle the step-to-int mapping in some way.
     model = load_model()
-    prediction_output = generate_steps(model, n_vocab, onset_array)
+    prediction_output = generate_steps(model, n_vocab, binary_onset_array)
+
     return prediction_output
 
 
@@ -23,7 +27,6 @@ def load_model():
     print("Loaded model from disk")
 
     return loaded_model
-
 
 
 def decode_step(num):
@@ -47,31 +50,51 @@ def generate_steps(model, n_vocab, onsets):
     """ Generate notes from the neural network based on a sequence of notes """
 
     #Start with 100 zeros
-    pattern = np.zeros(100)
+    pattern = [0] * 100
     prediction_output = []
 
     # generate steps for rest of song
     for step_index, ele in enumerate(onsets):
-        if ele == 0:
+        #print("element", step_index, ele, type(ele))
+        #print("pattern Length:", len(pattern))
+
+        if int(ele) == 0:
             prediction_output.append([0,0,0,0])
             pattern.append(0)
             pattern = pattern[1:len(pattern)]
 
-        elif ele == 1:
+        elif int(ele) == 1:
             #TODO: need to reshape before predicting
-            prediction_input = pattern
+            prediction_input = numpy.reshape(pattern, (1, len(pattern), 1))
+            prediction_input = prediction_input / float(n_vocab)
 
             prediction = model.predict(prediction_input, verbose=0)
 
             best_prediction = numpy.argmax(prediction)
             result = decode_step(best_prediction)
+
+            #print("result", step_index, result)
+
             prediction_output.append(result)
 
-            norm_best_pred = best_prediction / float(n_vocab)
-            pattern.append(norm_best_pred)
+            pattern.append(best_prediction)
             pattern = pattern[1:len(pattern)]
 
     return prediction_output
 
-#if __name__ == '__main__':
-    #generate()
+
+
+
+if __name__ == '__main__':
+    source = "../songs/sucker.wav"
+    n_vocab = 81
+    onsets = get_onsets(source)
+    bpm = get_bpm(source)
+    notes = onsets_to_notes(onsets, bpm, source)
+    num_16ths = len(times16ths(source, bpm))
+    binary_onset_array = numpy.zeros(num_16ths + 1)
+    for i in notes:
+        binary_onset_array[i] = 1
+    #print(notes)
+
+    print(generate(binary_onset_array, n_vocab))
